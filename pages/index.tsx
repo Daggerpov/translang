39bf9ef28d73a245d5588b00c7e6f50eed7dc835
Daggerpov <danielagapov1@gmail.com>
2022-06-13 13:33:05 -0400

    removing empty lines that turn into sole semicolons from language conversion to Java

--- pages/index.tsx
+++ pages/index.tsx
@@ -33,22 +33,40 @@ const Home: NextPage = () => {
         let codeOutputLines: Array<any>;
 
         if (languageTo === "Java"){
-            // could alternatively use RegEx (Regular Expressions) instead of the first argument being a string
-            codeOutput = codeOutput.replace("print", "System.out.println");
+            // ? could alternatively use RegEx (Regular Expressions) instead of the first
+            // ? argument being a string, but I don't believe I'll need this
+            codeOutput = codeOutput.replaceAll("print", "System.out.println");
 
             codeOutputLines = codeOutput.split(/[\r\n]+/); // RegEx for splitting by line
+
+            // * better way to add semicolon to each line then convert to string
+            codeOutputLines = codeOutputLines.map((item) => `${item};`);
+
+            console.log(codeOutputLines);
+
+            // this filters out the semicolon-only lines from the output after having added 
+            // these semicolons to every line with the language conversion (prevents empty lines
+            // at the end of the code block)
+            let filteredCodeOutputLines = codeOutputLines.filter(function (element) {
+                return element != ";";
+            });
+
+            codeOutput = filteredCodeOutputLines.join("\n");
+
+            console.log(codeOutput);
 
             // for (
             //     let lineNumber = 0;
             //     lineNumber < codeOutputLines.length;
             //     lineNumber++
             // ) {
-            //     codeOutputLines[lineNumber] += ";";
+            //     if (codeOutputLines[lineNumber] == ";") {
+            //         console.log("true");
+            //         codeOutput = codeOutputLines.slice(lineNumber).join("\n");
+            //     }
+                
             // }
-
-            // ? better way to add semicolon to each line then convert to string
-            codeOutput = codeOutputLines.map((item) => `${item};`).join("\n");
-            console.log(codeOutput);
+            // console.log(codeOutput);
         }
         
         if (codeInput != codeOutput) {
