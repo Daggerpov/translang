d1b238952429d79cdbdcdd93e1f94338e4058af5
TimothyDao <timothydao273@gmail.com>
2022-07-31 17:13:58 -0400

    started to implement inbox list

--- pages/menu/inbox.tsx
+++ pages/menu/inbox.tsx
@@ -4,15 +4,36 @@ import type { NextPage } from "next";
 import Image from "next/image";
 import styles from "../../styles/Home.module.css";
 
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+
 import React from "react";
 
+function generate(element: React.ReactElement) {
+    return [0, 1, 2, 3, 4, 5, 6, 7,8,9,10].map((value) =>
+      React.cloneElement(element, {
+        key: value,
+      }),
+    );
+  }
 
 const Inbox: NextPage = (users) => {
 
     return (
         <>
             <main className={styles.main}>
-                <p>inbox page</p>
+
+                <List align="left" style={{height: "80%", width: "100%"}}>
+                {generate(
+                    <ListItem style={{ border: "1px solid gray" }}>
+                        <ListItemText
+                            primary="Single-line item"
+                            secondary="Secondary text"
+                        />
+                    </ListItem>,
+                )}
+                </List>
             </main>
 
             <footer className={styles.footer}>
